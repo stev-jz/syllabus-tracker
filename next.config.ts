d5f29252af05710d23b1ca9dbeef310bd0fa56d7
Steven <stevenzhang.7161@gmail.com>
2025-09-04 23:20:42 -0400

    pushing all changes to transfer to another device

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  serverExternalPackages: ['@prisma/client', 'prisma']
+  serverExternalPackages: ['@prisma/client', 'prisma'],
+  experimental: {
+    serverComponentsExternalPackages: ['@prisma/client', 'prisma']
+  }
 };
 
 export default nextConfig;
