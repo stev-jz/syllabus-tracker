bd0066f8a47bab694544a21e1e1e721dfe57c58d
Steven Zhang <stevenzhang.7161@gmail.com>
2026-01-13 18:51:08 -0500

    prisma downgrade for previous windows behaviour

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,10 +1,10 @@
 import { PrismaClient } from '../generated/prisma'
 
-
 const globalForPrisma = globalThis as unknown as {
   prisma: PrismaClient | undefined
 }
 
+// Prisma 6: connection URL is in the schema file
 export const prisma = globalForPrisma.prisma ?? new PrismaClient()
 
 if (process.env.NODE_ENV !== 'production') {
